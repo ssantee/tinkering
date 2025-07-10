b78a30e6529a8b88f9d335928632e5bcfa2a828c
Steven Santee <steven.santee@marion.k12.fl.us>
2025-07-10 14:39:02 -0400

    output types

--- dom-recursion/js/main.js
+++ dom-recursion/js/main.js
@@ -9,19 +9,53 @@ class ElementTree {
     }
 }
 class TreeDisplayer {
-    constructor(root, character) {
+    constructor(root, character, outputMethod, printTarget) {
         this.character = "\t";
         this.character = character;
         this.root = root;
+        this.outputMethod = outputMethod ? outputMethod : "console";
+        this.printTarget = printTarget;
     }
-    print(elementName, depth) {
+    output(elementName, depth) {
         const ind = this.character.repeat(depth);
-        console.log(ind + elementName);
+        const outS = ind + elementName;
+        switch (this.outputMethod) {
+            case "console":
+                this.console(outS);
+                break;
+            case "text":
+                if (this.printTarget &&
+                    this.printTarget) {
+                    this.printTarget.value =
+                        this.printTarget.value +
+                            outS +
+                            "\n";
+                }
+                else {
+                    throw new Error(`${this.outputMethod} output method requires a target`);
+                }
+                break;
+            case "render":
+                if (this.printTarget &&
+                    this.printTarget) {
+                    let outEl = document.createElement("div");
+                    outEl.textContent = outS;
+                    outEl.style.paddingLeft = (depth * 12).toString() + "px";
+                    this.printTarget.appendChild(outEl);
+                }
+                else {
+                    throw new Error(`${this.outputMethod} output method requires a target`);
+                }
+                break;
+        }
+    }
+    console(s) {
+        console.log(s);
     }
     display(e, depth) {
         depth = depth === undefined ? 0 : depth + 1;
         e = e ? e : this.root;
-        this.print(e.displayString, depth);
+        this.output(e.displayString, depth);
         e.childElements &&
             e.childElements.forEach((e) => {
                 this.display(e, depth);
@@ -37,5 +71,13 @@ new TreeDisplayer({
             childElements: [{ displayString: "2nd tier child" }],
         },
     ],
-}, "\t").display();
+}, "____").display();
+let txtTarget = document.getElementById("inputTarget");
+if (txtTarget != null) {
+    new TreeDisplayer(new ElementTree(document.documentElement), "\t", "text", txtTarget).display();
+}
+let elTarget = document.getElementById("elementTarget");
+if (elTarget != null) {
+    new TreeDisplayer(new ElementTree(document.documentElement), "\t", "render", elTarget).display();
+}
 //# sourceMappingURL=main.js.map
--- dom-recursion/src/main.ts
+++ dom-recursion/src/main.ts
@@ -12,22 +12,72 @@ class ElementTree {
     }
 }
 
+type TreeOutputMethod = "console" | "text" | "render";
+
 class TreeDisplayer {
     private character: string = "\t";
     private root: Printable;
-    constructor(root: Printable, character: string) {
+    private outputMethod: TreeOutputMethod;
+    private printTarget?: ElementPrintTarget | InputPrintTarget;
+    constructor(
+        root: Printable,
+        character: string,
+        outputMethod?: TreeOutputMethod,
+        printTarget?: ElementPrintTarget | InputPrintTarget
+    ) {
         this.character = character;
         this.root = root;
+        this.outputMethod = outputMethod ? outputMethod : "console";
+        this.printTarget = printTarget;
     }
-    private print(elementName: string, depth: number) {
+    private output(elementName: string, depth: number) {
         const ind = this.character.repeat(depth);
-        console.log(ind + elementName);
+        const outS = ind + elementName;
+
+        switch (this.outputMethod) {
+            case "console":
+                this.console(outS);
+                break;
+            case "text":
+                if (
+                    this.printTarget &&
+                    (this.printTarget as InputPrintTarget)
+                ) {
+                    (this.printTarget as InputPrintTarget).value =
+                        (this.printTarget as InputPrintTarget).value +
+                        outS +
+                        "\n";
+                } else {
+                    throw new Error(
+                        `${this.outputMethod} output method requires a target`
+                    );
+                }
+                break;
+            case "render":
+                if (
+                    this.printTarget &&
+                    (this.printTarget as ElementPrintTarget)
+                ) {
+                    let outEl = document.createElement("div");
+                    outEl.textContent = outS;
+                    outEl.style.paddingLeft = (depth * 12).toString() + "px";
+                    (this.printTarget as ElementPrintTarget).appendChild(outEl);
+                } else {
+                    throw new Error(
+                        `${this.outputMethod} output method requires a target`
+                    );
+                }
+                break;
+        }
+    }
+    private console(s: string) {
+        console.log(s);
     }
     public display(e?: Printable, depth?: number) {
         depth = depth === undefined ? 0 : depth + 1;
         e = e ? e : this.root;
 
-        this.print(e.displayString, depth);
+        this.output(e.displayString, depth);
 
         e.childElements &&
             e.childElements.forEach((e) => {
@@ -39,6 +89,14 @@ class TreeDisplayer {
 interface Printable {
     displayString: string;
     childElements?: Array<Printable>;
+}
+
+interface ElementPrintTarget {
+    appendChild: Node["appendChild"];
+}
+
+interface InputPrintTarget {
+    value: string;
 }
 
 new TreeDisplayer(new ElementTree(document.documentElement), "\t").display();
@@ -53,5 +111,27 @@ new TreeDisplayer(
             },
         ],
     },
-    "\t"
+    "____"
 ).display();
+
+let txtTarget = document.getElementById("inputTarget");
+
+if (txtTarget != null) {
+    new TreeDisplayer(
+        new ElementTree(document.documentElement),
+        "\t",
+        "text",
+        txtTarget
+    ).display();
+}
+
+let elTarget = document.getElementById("elementTarget");
+
+if (elTarget != null) {
+    new TreeDisplayer(
+        new ElementTree(document.documentElement),
+        "\t",
+        "render",
+        elTarget
+    ).display();
+}
