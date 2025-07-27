c52c98b0a513373bf642821f2aefab1ac56bf08c
Steven Santee <ssantee@icloud.com>
2025-07-27 12:12:33 -0400

    changes

--- interaction/js/main.js
+++ interaction/js/main.js
@@ -1,21 +1,21 @@
 "use strict";
 class Follower {
-    constructor({ el, prop, proptemplate, templateKey }) {
-        this.templateKey = '{templateKey}';
+    constructor({ el }) {
+        this.prop = 'transform';
+        this.proptemplate = 'rotateZ({val}deg)';
+        this.templateKey = '{val}';
         this.element = el;
         this.position = this.initPosition();
-        this.prop = prop;
-        this.proptemplate = proptemplate;
-        if (templateKey) {
-            this.templateKey = templateKey;
-        }
-        console.log(this.position);
     }
     initPosition() {
         let l1 = this.element;
         // center of the indicator element
         // this is where the follower will rotate around
-        return { x: l1.offsetLeft + (l1.offsetWidth / 2), y: l1.offsetTop + (l1.offsetHeight / 2), rotation: 0 };
+        return {
+            x: l1.offsetLeft + l1.offsetWidth / 2,
+            y: l1.offsetTop + l1.offsetHeight / 2,
+            rotation: 0,
+        };
     }
     Follow(item) {
         // get the current position of the item to follow
@@ -26,21 +26,28 @@ class Follower {
         }
         // calculate the angle between the follower and the item
         // atan2 gives us the angle in radians, we convert it to degrees
-        let a = Math.atan2((mp.y - this.position.y), (mp.x - this.position.x)) * 180 / Math.PI;
+        let a = (Math.atan2(mp.y - this.position.y, mp.x - this.position.x) * 180) /
+            Math.PI;
         // and adjust it by 90 degrees to align with the CSS rotation
         a += 90;
-        this.element.style[this.prop] = this.proptemplate.replace(this.templateKey, a.toString());
+        this.element.style.transform =
+            this.proptemplate.replace(this.templateKey, a.toString());
+        this.position.rotation = a;
     }
 }
 class PointerTracker {
     constructor() {
+        this.mmove = (event) => {
+            this.pointerPosition = { x: event.pageX, y: event.pageY };
+        };
         this.pointerPosition = { x: 0, y: 0 };
         this.initHandler();
     }
     initHandler() {
-        document.addEventListener('mousemove', (event) => {
-            this.pointerPosition = { x: event.pageX, y: event.pageY };
-        });
+        document.addEventListener('mousemove', this.mmove);
+    }
+    Stop() {
+        document.removeEventListener('mousemove', this.mmove);
     }
     GetCurrentPosition() {
         return this.pointerPosition;
--- interaction/src/main.ts
+++ interaction/src/main.ts
@@ -2,30 +2,28 @@ type IndicatorPosition = {
     x: number;
     y: number;
     rotation: number;
-}
+};
 
 class Follower {
-    private element: Element
-    private position: IndicatorPosition
-    private prop: string
-    private proptemplate: string
-    private templateKey: string = '{templateKey}';
-    constructor({ el, prop, proptemplate, templateKey }: { el: Element, prop: string, proptemplate: string, templateKey?: string }) {
+    private element: Element;
+    private position: IndicatorPosition;
+    private prop: string = 'transform';
+    private proptemplate: string = 'rotateZ({val}deg)';
+    private templateKey: string = '{val}';
+    constructor({ el }: { el: Element }) {
         this.element = el;
         this.position = this.initPosition();
-        this.prop = prop;
-        this.proptemplate = proptemplate;
-        if (templateKey) {
-            this.templateKey = templateKey;
-        }
-        console.log(this.position)
     }
 
     private initPosition(): IndicatorPosition {
         let l1 = this.element as HTMLElement;
         // center of the indicator element
         // this is where the follower will rotate around
-        return { x: l1.offsetLeft + (l1.offsetWidth / 2), y: l1.offsetTop + (l1.offsetHeight / 2), rotation: 0 }
+        return {
+            x: l1.offsetLeft + l1.offsetWidth / 2,
+            y: l1.offsetTop + l1.offsetHeight / 2,
+            rotation: 0,
+        };
     }
 
     public Follow(item: FollowTarget) {
@@ -37,17 +35,22 @@ class Follower {
         }
         // calculate the angle between the follower and the item
         // atan2 gives us the angle in radians, we convert it to degrees
-        let a = Math.atan2((mp.y - this.position.y), (mp.x - this.position.x)) * 180 / Math.PI;
+        let a =
+            (Math.atan2(mp.y - this.position.y, mp.x - this.position.x) * 180) /
+            Math.PI;
         // and adjust it by 90 degrees to align with the CSS rotation
         a += 90;
-        (this.element as HTMLElement).style[this.prop as any] = this.proptemplate.replace(this.templateKey, a.toString());
+
+        (this.element as HTMLElement).style.transform =
+            this.proptemplate.replace(this.templateKey, a.toString());
+        this.position.rotation = a;
     }
 }
 
-type Coordinate = { x: number, y: number }
+type Coordinate = { x: number; y: number };
 
 interface FollowTarget {
-    GetCurrentPosition(): Coordinate
+    GetCurrentPosition(): Coordinate;
 }
 
 class PointerTracker {
@@ -57,10 +60,14 @@ class PointerTracker {
         this.pointerPosition = { x: 0, y: 0 };
         this.initHandler();
     }
+    private mmove = (event: MouseEvent) => {
+        this.pointerPosition = { x: event.pageX, y: event.pageY };
+    };
     private initHandler() {
-        document.addEventListener('mousemove', (event) => {
-            this.pointerPosition = { x: event.pageX, y: event.pageY };
-        });
+        document.addEventListener('mousemove', this.mmove);
+    }
+    public Stop() {
+        document.removeEventListener('mousemove', this.mmove);
     }
     public GetCurrentPosition(): Coordinate {
         return this.pointerPosition;
